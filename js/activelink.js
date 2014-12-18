e0f07924a69cae6ad967aad12733951d152b1f5c
Matteo Scarpa <matteoscarpa92@gmail.com>
2014-12-18 16:13:21 +0100

    Test js

--- js/activelink.js
+++ js/activelink.js
@@ -7,7 +7,7 @@ function setNavigation() {
     path = path.replace(/\/$/, "");
     path = decodeURIComponent(path);
 
-    $(".nav a").each(function () {
+    $('a').each(function () {
         var href = $(this).attr('href');
         if (path.substring(0, href.length) === href) {
             $(this).closest('li').addClass('active');
