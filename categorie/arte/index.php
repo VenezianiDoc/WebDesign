e42eb603c4d917333a8d887ff589f538364e4864
Matteo Scarpa <matteoscarpa92@gmail.com>
2014-12-19 21:20:34 +0100

    Conversione php
    Modifica dei file per avere tutto in php. Eliminato codice extra duplicato nei php

---
+++ categorie/arte/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <?php
+    include "../../head.php"
+    ?>
+    <title>Arte - Discover Veneto</title>
+</head>
+
+<body>
+
+
+<!-- ========= CONTAINER ======== -->
+<div class="container">
+    <!-- breadcrumbs -->
+    <ol class="breadcrumb">
+        <li><a href="/index.php">Home</a>
+        </li>
+        <li><a href="/categorie/">Categorie</a>
+        </li>
+        <!-- pagina attiva-->
+        <li class="active">Arte</li>
+    </ol>
+</div>
+<!-- /breadcrumbs -->
+<div class="container">
+    <div class="page-header">
+        <h1>Arte
+            <small> Scopri tutte le offerte</small>
+        </h1>
+    </div>
+
+    <div class="container">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consequat cursus ex, et dignissim lectus
+            pellentesque sit amet. Nunc fermentum nisl id mi molestie sagittis. Curabitur pharetra, lacus et ornare
+            placerat, nisi orci lobortis sem, id auctor metus ex eget nulla. Sed hendrerit elementum mi, in sollicitudin
+            est molestie nec. Ut volutpat vulputate lacus in pretium. Aliquam consequat dolor tortor, in imperdiet diam
+            posuere quis. Vivamus ex metus, finibus eu lectus in, consequat vehicula odio. Aenean imperdiet felis urna,
+            et blandit orci accumsan in. CIAOOOOOOO</p>
+
+        <p>Donec tempor, odio eu pretium consectetur, massa dui varius odio, viverra eleifend mauris urna id lacus. Cras
+            molestie pellentesque magna, in efficitur eros vestibulum tincidunt. Maecenas porta rhoncus iaculis. Ut sit
+            amet lorem nec ipsum semper placerat in nec urna. Integer dictum enim in justo rhoncus, et hendrerit orci
+            rhoncus. Aenean pretium eget urna id finibus. Suspendisse vel commodo nibh. Pellentesque habitant morbi
+            tristique senectus et netus et malesuada fames ac turpis egestas. Interdum et malesuada fames ac ante ipsum
+            primis in faucibus. Praesent hendrerit nulla consequat semper sagittis. Duis ut sollicitudin magna.</p>
+
+        <p>Sed lobortis consectetur neque, nec accumsan ligula interdum fringilla. Morbi laoreet, ipsum non efficitur
+            aliquet, elit leo vehicula urna, tincidunt tempor ex risus non dolor. Duis ac bibendum eros. Nunc id posuere
+            lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan turpis sed urna pharetra, non
+            varius tellus venenatis. Donec at porta sem. Donec id dignissim dolor. Cras non diam quam. Curabitur nec
+            lectus vehicula, porta eros sed, ultrices metus. Integer ut mauris lectus. Fusce sollicitudin rutrum urna id
+            lobortis. Integer pellentesque velit et mollis accumsan. Sed semper ex ac tellus lobortis, vitae posuere
+            velit condimentum. Donec suscipit blandit turpis in pellentesque. Ut pretium suscipit sapien et
+            vulputate.</p>
+
+
+    </div>
+    <!-- ========= ./CONTAINER ======== -->
+
+
+    <!-- FOOTER -->
+    <div id="footer"></div>
+
+    <!-- ./FOOTER -->
+
+    <!-- Script -->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../../js/bootstrap.min.js"></script>
+</div>
+</body>
+
+</html>
