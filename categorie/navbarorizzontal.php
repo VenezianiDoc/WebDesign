f0a01f010a8f93d915fb6812e47433d7bf0814e8
Andrea <andreafrison@gmail.com>
2014-12-20 12:47:25 +0100

    navor in lista

--- categorie/navbarorizzontal.php
+++ categorie/navbarorizzontal.php
@@ -1,12 +1,9 @@
-<div class="btn-group-vertical" role="group" aria-label="Categorie">
-    <div class="btn-group" role="group">
-            <a type="button" class="btn btn-default" href="/categorie/index.php">Indice categorie</a>
-            <a type="button" class="btn btn-default" href="/categorie/avventura">Avventura</a>
-            <a type="button" class="btn btn-default" href="/categorie/relax">Relax &amp; Benessere</a>
-            <a type="button" class="btn btn-default" href="/categorie/arte">Arte</a>
-            <a type="button" class="btn btn-default" href="/categorie/famiglia">Famiglia</a>
-            <a type="button" class="btn btn-default" href="/categorie/studenti">Studenti</a>
-            <a type="button" class="btn btn-default" href="/categorie/business">Business Man</a>
-
-    </div>
-</div>
+<ul class="navor" role="group" aria-label="Categorie">
+            <li><a href="/categorie/index.php">Indice categorie</a></li>
+            <li><a href="/categorie/avventura">Avventura</a></li>
+            <li><a href="/categorie/relax">Relax &amp; Benessere</a></li>
+            <li><a href="/categorie/arte">Arte</a></li>
+            <li><a href="/categorie/famiglia">Famiglia</a></li>
+            <li><a href="/categorie/studenti">Studenti</a></li>
+            <li><a href="/categorie/business">Business Man</a></li>
+</ul>
