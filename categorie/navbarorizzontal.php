c6f427750e9b7def72d360e8c1512799a51c2e95
Matteo Scarpa <matteoscarpa92@gmail.com>
2014-12-20 10:24:03 +0100

    Aggiunto menù secondario per le categorie

---
+++ categorie/navbarorizzontal.php
@@ -0,0 +1,37 @@
+<div id="navigation_bar">
+    <nav class="navbar navbar-inverse">
+        <div class="container">
+            <!-- Header della navbar -->
+            <div class="navbar-header">
+                <!-- Pulsanti per v. mobile -->
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+        </div>
+        <div class="container">
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-left">
+                    <li>
+                        <a href="/categorie/index.php">Indice categorie</a>
+                    </li>
+                    <li><a href="/categorie/avventura">Avventura</a>
+                    </li>
+                    <li><a href="/categorie/relax">Relax &amp; Benessere</a>
+                    </li>
+                    <li><a href="/categorie/arte">Arte</a>
+                    </li>
+                    <li><a href="/categorie/famiglia">Famiglia</a>
+                    </li>
+                    <li><a href="/categorie/studenti">Studenti</a>
+                    </li>
+                    <li><a href="/categorie/business">Business Man</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
+<!-- ========= ./NAVBAR ========= -->
