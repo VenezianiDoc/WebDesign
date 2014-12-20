19033475027cbdd35bc6b2df76c3d019b76ae4b2
Andrea <andreafrison@gmail.com>
2014-12-20 23:39:09 +0100

    Viaggiare sicuri

---
+++ viaggiaresicuri/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <?php include '../head.php' ?>
+    <title>Categorie - Discover Veneto</title>
+</head>
+
+<body>
+
+    <?php include '../navbar.php' ?>
+
+    <!-- ========= CONTAINER ======== -->
+    <div class="container">
+        <!-- breadcrumbs -->
+        <ol class="breadcrumb">
+            <li><a href="../index">Home</a>
+            </li>
+            <!-- pagina attiva-->
+            <li class="active">Viaggiare Sicuri</li>
+        </ol>
+    </div>
+    <!-- /breadcrumbs -->
+
+    <div class="container">
+        <div class="page-header">
+            <h1>Viaggiare sicuri
+            <small> Non rovinarti la vacanza!</small>
+        </h1>
+        </div>
+
+        <div class="container">
+            <h2><a href="comefare.php">Come fare se..</a></h2>
+            <br>
+            <p>Guida rapida su come affrontare gli imprevisti nel viaggio >>>> DA INSERIRE IMMAGINI O ALTRO</p>
+
+            <h2><a href="utili.php">Numeri utili</a></h2>
+            <br>
+            <p>Breve elenco di tutti i numeri utili per viaggiare sicuri >>>> DA INSERIRE IMMAGINI O ALTRO</p>
+
+
+        </div>
+    </div>
+    <!-- ========= ./CONTAINER ======== -->
+
+
+    <?php include '../footer.php' ?>
+
+</body>
+
+</html>
