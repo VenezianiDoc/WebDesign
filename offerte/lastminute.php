1d339edbbe4a2df921915a69146ac470bb901959
Andrea <andreafrison@gmail.com>
2014-12-20 22:30:49 +0100

    Bassa stagione e Last minute

--- offerte/lastminute.php
+++ offerte/lastminute.php
@@ -1,8 +1,55 @@
-last minute
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <?php include '../head.php' ?>
+    <title>Bassa stagione - Discover Veneto</title>
+</head>
+
+<body>
+
+    <?php include '../navbar.php' ?>
+
+    <!-- ========= CONTAINER ======== -->
+    <div class="container">
+        <!-- breadcrumbs -->
+        <ol class="breadcrumb">
+            <li>
+                <a href="../index.php">Home</a>
+            </li>
+            <li>
+                <a href="index.php">Offerte</a>
+            </li>
+            <!-- pagina attiva-->
+            <li class="active">Last minute</li>
+        </ol>
+    </div>
+    <!-- /breadcrumbs -->
+    <div class="container">
+        <div class="page-header">
+            <h1>Last minute
+            <small> Offerte fino all'ultimo secondo!</small>
+        </h1>
+        </div>
+
+        <div class="container">
+            <h2>Qualcosa</h2>
+            <br>
+            <p>Tutte le offerte! >>>> DA INSERIRE IMMAGINI O ALTRO</p>
+
+            <h2>Qualcosa</h2>
+            <br>
+            <p>I prezzi migliori solo per te! >>>> DA INSERIRE IMMAGINI O ALTRO</p>
+
+
+
+        </div>
     </div>
     <!-- ========= ./CONTAINER ======== -->
-    
-    
+
+
     <?php include '../footer.php' ?>
-    
+
 </body>
+
+</html>
