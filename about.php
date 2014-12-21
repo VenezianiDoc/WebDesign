589bc49365544f994d0693bcd983ed232cde8193
Andrea <andreafrison@gmail.com>
2014-12-21 17:48:29 +0100

    Creazione pagina about us

--- about.php
+++ about.php
@@ -1,7 +1,43 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <?php include 'head.php' ?>
+    <title>About uss - Discover Veneto</title>
+</head>
+
+<body>
+
+    <?php include 'navbar.php' ?>
+
+    <!-- ========= CONTAINER ======== -->
+    <div class="container">
+        <!-- breadcrumbs -->
+        <ol class="breadcrumb">
+            <li><a href="index.php">Home</a>
+            </li>
+            <!-- pagina attiva-->
+            <li class="active">About us</li>
+        </ol>
     </div>
+    <!-- /breadcrumbs -->
+
+    <div class="container">
+        <div class="page-header">
+            <h1>About us
+            <small> Chi siamo</small>
+        </h1>
+        </div>
+
+        <div class="container">
+            <h2>Veneziani Doc</h2>
+            <p>Siamo un gruppo di studenti universitari del corso di Informatica di Ca' Foscari che studiano, vivono e lavorano a Venezia</p>
+        </div>
+    </div>
+
     <!-- ========= ./CONTAINER ======== -->
-    
-    
+
+
     <?php include 'footer.php' ?>
-    
+
 </body>
