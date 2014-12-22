81b7346c2e575707d852dd12ef0c6f91e7e710b6
Matteo Scarpa <matteoscarpa92@gmail.com>
2014-12-22 09:22:04 +0100

    add slide

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <?php include 'navbar.php' ?>
 
     <!-- ========= CAROUSEL ========= -->
-    <div id="myCarousel" class="carousel hidden-print">
+    <div id="myCarousel" class="carousel slide hidden-print">
         <!-- Indicators -->
         <ol class="carousel-indicators">
             <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
