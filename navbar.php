8bb957fd0e65a0de5105d83075fc899cab32de70
Andrea <andreafrison@gmail.com>
2014-12-23 21:34:41 +0100

    Last minute

--- navbar.php
+++ navbar.php
@@ -71,7 +71,7 @@
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
                     <li>
-                    <?php if($_SESSION['user']==null){echo '<a href="/user/login.php">Accedi</a>';} else{echo '<a href="/user/index.php">Account di '.$_SESSION['user'].'</a>';} ?>
+                    <?php if($_SESSION['user']==null){echo '<a href="/user/login.php">Accedi</a>';} elseif($_SESSION['user']==' '){echo '<a href="/user/index.php">Il tuo acconut</a>';} else{echo '<a href="/user/index.php">Account di '.$_SESSION['user'].'</a>';} ?>
                     </li>
                     <li><a href="/carrello"><i class="fa fa-shopping-cart"></i> Carrello</a>
                     </li>                    
--- offerte/lastminute.php
+++ offerte/lastminute.php
@@ -4,6 +4,7 @@
 <head>
     <?php include '../head.php' ?>
     <title>Bassa stagione - Discover Veneto</title>
+    <link href="css/style.css" rel="stylesheet">
 </head>
 
 <body>
@@ -33,14 +34,102 @@
         </div>
 
         <div class="container">
-            <h2>Qualcosa</h2>
-            <p>Tutte le offerte! >>>> DA INSERIRE IMMAGINI O ALTRO</p>
+            
+            <div class="row">
+    			<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+					<!-- PRICE ITEM -->
+					<div class="panel price panel-red">
+						<div class="panel-heading  text-center">
+						<h3>NOTTE A VENZIA</h3>
+						</div>
+						<div class="panel-body text-center">
+							<p class="lead" style="font-size:40px"><strong>159 €</strong></p>
+						</div>
+						<ul class="list-group list-group-flush text-center">
+							<li class="list-group-item"><i class="icon-ok text-danger"></i> 1 Notte</li>
+							<li class="list-group-item"><i class="icon-ok text-danger"></i> Camera doppia</li>
+							<li class="list-group-item"><i class="icon-ok text-danger"></i> Hotel 5 stelle</li>
+						</ul>
+						<div class="panel-footer">
+							<a class="btn btn-lg btn-block btn-danger" href="#">Compra ora!</a>
+						</div>
+					</div>
+					<!-- /PRICE ITEM -->
+                </div>
+				
+				<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+				    <!-- PRICE ITEM -->
+					<div class="panel price panel-blue">
+						<div class="panel-heading arrow_box text-center">
+						<h3>ABANO TERME</h3>
+						</div>
+						<div class="panel-body text-center">
+							<p class="lead" style="font-size:40px"><strong>109 €</strong></p>
+						</div>
+						<ul class="list-group list-group-flush text-center">
+							<li class="list-group-item"><i class="icon-ok text-info"></i> SPA per 2 persone</li>
+							<li class="list-group-item"><i class="icon-ok text-info"></i> Massaggi inclusi</li>
+							<li class="list-group-item"><i class="icon-ok text-info"></i> 5 stelle</li>
+						</ul>
+						<div class="panel-footer">
+							<a class="btn btn-lg btn-block btn-info" href="#">BUY NOW!</a>
+						</div>
+					</div>
+					<!-- /PRICE ITEM -->
+				</div>
+				
+				<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+					<!-- PRICE ITEM -->
+					<div class="panel price panel-green">
+						<div class="panel-heading arrow_box text-center">
+						<h3>NOTTE IN RIFUGIO</h3>
+						</div>
+						<div class="panel-body text-center">
+							<p class="lead" style="font-size:40px"><strong>59 €</strong></p>
+						</div>
+						<ul class="list-group list-group-flush text-center">
+							<li class="list-group-item"><i class="icon-ok text-success"></i> Parco Naturale Dolomiti</li>
+							<li class="list-group-item"><i class="icon-ok text-success"></i> Assicurazione CAI</li>
+							<li class="list-group-item"><i class="icon-ok text-success"></i> Mezza pensione</li>
+						</ul>
+						<div class="panel-footer">
+							<a class="btn btn-lg btn-block btn-success" href="#">BUY NOW!</a>
+						</div>
+					</div>
+					<!-- /PRICE ITEM -->
+				</div>
+				
+				<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+                    <!-- PRICE ITEM -->
+					<div class="panel price panel-grey">
+						<div class="panel-heading arrow_box text-center">
+						<h3>GIRO IN GONDOLA</h3>
+						</div>
+						<div class="panel-body text-center">
+							<p class="lead" style="font-size:40px"><strong>80 €</strong></p>
+						</div>
+						<ul class="list-group list-group-flush text-center">
+							<li class="list-group-item"><i class="icon-ok text-success"></i> Tour Canal Grande</li>
+							<li class="list-group-item"><i class="icon-ok text-success"></i> Per 2 persone</li>
+							<li class="list-group-item"><i class="icon-ok text-success"></i> 50 minuti</li>
+						</ul>
+						<div class="panel-footer">
+							<a class="btn btn-lg btn-block btn-primary" href="#">BUY NOW!</a>
+						</div>
+					</div>
+					<!-- /PRICE ITEM -->
+                </div>
+				
 
-            <h2>Qualcosa</h2>
-            <p>I prezzi migliori solo per te! >>>> DA INSERIRE IMMAGINI O ALTRO</p>
-
-
-
+			</div>
+            
+            <div class="row text-center">
+                Caricamento altre offerte
+            </div>
+            
+            
+            
+            
         </div>
     </div>
     <!-- ========= ./CONTAINER ======== -->
