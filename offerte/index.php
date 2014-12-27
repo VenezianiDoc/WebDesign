10cdcac0d794cfadbacb9f00a7478599e6a182ae
Andrea <andreafrison@gmail.com>
2014-12-27 12:05:59 +0100

    Aggiunta link agli item da offerte

--- offerte/index.php
+++ offerte/index.php
@@ -8,53 +8,168 @@
 
 <body>
 
-<?php include '../navbar.php' ?>
+    <?php include '../navbar.php' ?>
 
-<!-- ========= CONTAINER ======== -->
-<div class="container">
-    <!-- breadcrumbs -->
-    <ol class="breadcrumb">
-        <li><a href="../index.php">Home</a>
-        </li>
-        <!-- pagina attiva-->
-        <li class="active">Offerte</li>
-    </ol>
-</div>
-<!-- /breadcrumbs -->
-<div class="container">
-    <div class="page-header">
-        <h1>Offerte
+    <!-- ========= CONTAINER ======== -->
+    <div class="container">
+        <!-- breadcrumbs -->
+        <ol class="breadcrumb">
+            <li><a href="../index.php">Home</a>
+            </li>
+            <!-- pagina attiva-->
+            <li class="active">Offerte</li>
+        </ol>
+    </div>
+    <!-- /breadcrumbs -->
+    <div class="container">
+        <div class="page-header">
+            <h1>Offerte
             <small> Un mondo di offerte tutte per te!</small>
         </h1>
-    </div>
+        </div>
 
-    <div class="container text-center">
-        <div class="col-lg-12">
-            <a href="lastminute.php"><h2>Last minute</h2>
+        <div class="container text-center">
+            <div class="col-lg-12">
+                <a href="lastminute.php"><h2>Last minute</h2>
 
                 <p><img src="/img/LastMinute_696x254.jpg" class="img-responsive img-center" alt="Last minute"></p></a>
 
-            <a href="bassa.php"><h2>Bassa stagione</h2>
+                <a href="bassa.php"><h2>Bassa stagione</h2>
 
                 <p><img src="/img/BassaStagione_696x254.jpg" class="img-responsive img-center" alt="Bassa stagione"></p></a>
 
+            </div>
         </div>
-    </div>
+        <br>
 
-    <div class="container text-center"><strong>Link a offerte migliori che portano direttamente alla pagina prodotto..
-            tipo /musei/item1.php</strong>
-    </div>
-    
-    <div class="row"><br>
-        <h3>Viaggiare sicuri</h3><hr>
-        <p>Prima di intraprendere un viaggio consulta le info fornite nella pagina <a href="/viaggiaresicuri/comefare.php">Come fare se</a> e porta sempre con te i numeri presenti nella pagina <a href="/viaggiaresicuri/utili.php">Numeri utili</a>.</p>
-    </div>
-    
-</div>
-<!-- ========= ./CONTAINER ======== -->
+        <div class="container">
+            <div class="row">
+                <br>
+                <h3>Offerte consigliate</h3>
+                <hr>
+                <div class="well">
+                    <div class="media">
+
+                        <div class="media-body">
+                            <a href="item1.php"><h4 class="media-heading">Offerta 1</h4></a>
+
+                            <p class="text-right">Città</p>
+
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero <a href="/destinazioni/musei/item1.php">[&hellip;]</a>
+                            </p>
+                            <ul class="list-inline list-unstyled">
+                                <li><span><i class="glyphicon glyphicon-calendar"></i> fino al 12 febbraio</span>
+                                </li>
+                                <li>|</li>
+                                <span><i class="glyphicon glyphicon-comment"></i> 2 commenti</span>
+                                <li>|</li>
+                                <li>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star-empty"></span>
+                                </li>
+                                <li>|</li>
+                                <li>
+                                    <!-- Use Font Awesome http://fortawesome.github.io/Font-Awesome/ -->
+                                    <span><i class="fa fa-facebook-square"></i></span>
+                                    <span><i class="fa fa-twitter-square"></i></span>
+                                    <span><i class="fa fa-google-plus-square"></i></span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="well">
+                    <div class="media">
+
+                        <div class="media-body">
+                            <a href="item1.php"><h4 class="media-heading">Offerta 2</h4></a>
+
+                            <p class="text-right">Città</p>
+
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero <a href="/destinazioni/musei/item1.php">[&hellip;]</a>
+                            </p>
+                            <ul class="list-inline list-unstyled">
+                                <li><span><i class="glyphicon glyphicon-calendar"></i> fino al 17 febbraio</span>
+                                </li>
+                                <li>|</li>
+                                <span><i class="glyphicon glyphicon-comment"></i> 9 commenti</span>
+                                <li>|</li>
+                                <li>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star-empty"></span>
+                                    <span class="glyphicon glyphicon-star-empty"></span>
+                                </li>
+                                <li>|</li>
+                                <li>
+                                    <!-- Use Font Awesome http://fortawesome.github.io/Font-Awesome/ -->
+                                    <span><i class="fa fa-facebook-square"></i></span>
+                                    <span><i class="fa fa-twitter-square"></i></span>
+                                    <span><i class="fa fa-google-plus-square"></i></span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="well">
+                    <div class="media">
+
+                        <div class="media-body">
+                            <a href="item1.php"><h4 class="media-heading">Offerta 3</h4></a>
+
+                            <p class="text-right">Città</p>
+
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero <a href="/destinazioni/musei/item1.php">[&hellip;]</a>
+                            </p>
+                            <ul class="list-inline list-unstyled">
+                                <li><span><i class="glyphicon glyphicon-calendar"></i> fino al 23 febbraio</span>
+                                </li>
+                                <li>|</li>
+                                <span><i class="glyphicon glyphicon-comment"></i> 5 commenti</span>
+                                <li>|</li>
+                                <li>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star-empty"></span>
+                                    <span class="glyphicon glyphicon-star-empty"></span>
+                                </li>
+                                <li>|</li>
+                                <li>
+                                    <!-- Use Font Awesome http://fortawesome.github.io/Font-Awesome/ -->
+                                    <span><i class="fa fa-facebook-square"></i></span>
+                                    <span><i class="fa fa-twitter-square"></i></span>
+                                    <span><i class="fa fa-google-plus-square"></i></span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
 
 
-<?php include '../footer.php' ?>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <br>
+                <h3>Viaggiare sicuri</h3>
+                <hr>
+                <p>Prima di intraprendere un viaggio consulta le info fornite nella pagina <a href="/viaggiaresicuri/comefare.php">Come fare se</a> e porta sempre con te i numeri presenti nella pagina <a href="/viaggiaresicuri/utili.php">Numeri utili</a>.</p>
+            </div>
+        </div>
+
+    </div>
+    <!-- ========= ./CONTAINER ======== -->
+
+
+    <?php include '../footer.php' ?>
 
 </body>
 
