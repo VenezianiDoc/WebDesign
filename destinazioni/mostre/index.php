bc4f5bccaec613d7aac1659242d400849d4e0313
Andrea <andreafrison@gmail.com>
2015-01-02 15:57:03 +0100

    hidden-print bug corretto

--- destinazioni/mostre/index.php
+++ destinazioni/mostre/index.php
@@ -32,7 +32,7 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-sm-2">
+            <div class="col-sm-2 hidden-print">
                 <?php include '../navbarorizzontal.php' ?>
             </div>
 
