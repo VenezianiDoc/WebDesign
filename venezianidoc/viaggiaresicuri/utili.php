93860bd3260d507bc59f4be87711087d2552bc1c
Matteo Scarpa <matteoscarpa92@gmail.com>
2015-01-17 18:58:24 +0100

    Fine

--- venezianidoc/viaggiaresicuri/utili.php
+++ venezianidoc/viaggiaresicuri/utili.php
@@ -2,13 +2,13 @@
 <html lang="it">
 
 <head>
-    <?php include '../head.php' ?>
+    <?php include '../headsecliv.php' ?>
     <title>Utili - Discover Veneto</title>
 </head>
 
 <body>
 
-<?php include '../navbar.php' ?>
+<?php include '../navbarsecliv.php' ?>
 
 <!-- ========= CONTAINER ======== -->
 <div class="container">
@@ -69,7 +69,7 @@
 <!-- ========= ./CONTAINER ======== -->
 
 
-<?php include '../footer.php' ?>
+<?php include '../footersecliv.php' ?>
 
 </body>
 
